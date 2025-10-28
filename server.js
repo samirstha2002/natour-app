b53ac4bf4fcef06bce77bb89901c74322e9d8588
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2025-10-28 14:37:39 +0545

    hhh

--- server.js
+++ server.js
@@ -18,6 +18,37 @@ mongoose
     console.error("DB connection error:", err);
   });
 
+const tourschema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "a tour must have name"],
+    unique: true,
+  },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "a tour must have price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourschema);
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 497,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log("error:", err);
+  });
 const app = require("./app");
 
 const port = process.env.PORT || 3000;
