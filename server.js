e3c7935b555a7cea995edbbbbe7099fc76b0abdc
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2025-10-28 10:59:02 +0545

    todays commit

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, // keep this
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
 
 const app = require("./app");
 
