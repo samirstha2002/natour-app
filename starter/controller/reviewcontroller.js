9343c0f0066ce0d15880e1d373e9df835164ec48
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2025-11-10 12:24:46 +0545

    okok

---
+++ starter/controller/reviewcontroller.js
@@ -0,0 +1,38 @@
+const AppError = require('../utils/appError');
+const Review = require('./../models/reviewmodel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getallreviews = catchAsync(async (req, res, next) => {
+  const reviews = Review.find();
+  res.status(200).json({
+    status: 'sucess',
+    result: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+exports.getreview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+  if (!review) {
+    return next(new AppError('No review  found with that Id', 404));
+  }
+  res.status(200).json({
+    status: 'sucess',
+    // results: tours.length,
+    data: {
+      review,
+    },
+  });
+});
+
+exports.createreview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: 'sucess',
+    data: {
+      review: newReview,
+    },
+  });
+});
