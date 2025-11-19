ea2e86b1b5968f6786be75de82ef870a31757b93
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2025-11-19 19:29:00 +0545

    log in functionality

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 
 const mongoSanitize = require('express-mongo-sanitize');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 // xss ko baki xa paxi grumla
 
 const morgan = require('morgan');
@@ -53,6 +54,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 // body parser ,reading the data from req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // data sanitization against NosqlInjection
 app.use((req, res, next) => {
@@ -72,6 +74,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
